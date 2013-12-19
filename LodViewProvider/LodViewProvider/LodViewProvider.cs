2df10333ee07b5a1a1cbfe9e78e59638d9ceebc6
Hiroyuki Inoue <mammymax@gmail.com>
2013-12-19 21:13:25 +0900

    Add LodViewProvider class

---
+++ LodViewProvider/LodViewProvider/LodViewProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LodViewProvider {
+	class LodViewProvider : QueryProvider {
+		public override string GetQueryText( System.Linq.Expressions.Expression expression ) {
+			return string.Empty;
+		}
+
+		public override object Execute( System.Linq.Expressions.Expression expression ) {
+			return null;
+		}
+
+		public static IQueryable<T> CreateQueryable<T>() {
+			return new Query<T>( new LodViewProvider() );
+		}
+	}
+}
