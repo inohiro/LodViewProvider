bd9bef9c05419a776ab90d2a0da50a3acbd83680
Hiroyuki Inoue <mammymax@gmail.com>
2013-12-19 21:27:02 +0900

    Tested Creating Basic Expression Tree

--- LodViewProvider/LodViewProvider/LodViewProvider.cs
+++ LodViewProvider/LodViewProvider/LodViewProvider.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 
 namespace LodViewProvider {
-	class LodViewProvider : QueryProvider {
+	public class LodViewProvider : QueryProvider {
 		public override string GetQueryText( System.Linq.Expressions.Expression expression ) {
 			return string.Empty;
 		}
---
+++ LodViewProvider/ProviderApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LodViewProvider;
+
+namespace ProviderApp {
+	class Program {
+		static void Main( string[] args ) {
+			var provider = LodViewProvider.LodViewProvider.CreateQueryable<int>();
+
+			var query = provider.Select( e => e == 1 );
+			Console.WriteLine( query.Expression );
+
+			Console.ReadKey();
+		}
+	}
+}
